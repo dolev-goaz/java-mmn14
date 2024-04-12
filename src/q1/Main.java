a2530fa3c4b49155a0e471176056e43743ee4b09
dolev goaz <www.dolev@gmail.com>
2024-04-12 17:30:42 +0300

    build and reverse list

--- src/q1/Main.java
+++ src/q1/Main.java
@@ -1,7 +1,46 @@
 package q1;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final int INPUT_COUNT = 6;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        LinkedList<String> list = new LinkedList<String>();
+        for (int i = 0; i < INPUT_COUNT; i++) {
+            list.add(receiveString("Insert a string: "));
+        }
+        System.out.println(list);
+        LinkedList<String> reversed = reverseList(list);
+        System.out.println(reversed);
     }
+
+    private static <T> LinkedList<T> reverseList(LinkedList<T> source) {
+        LinkedListItem<T> current = source.getHead();
+        LinkedListItem<T> previous = null;
+        LinkedListItem<T> next;
+
+        while (current != null) {
+            next = current.getNext();
+            current.setNext(previous);
+            previous = current;
+            current = next;
+        }
+
+        // now previous is the new head
+        LinkedList<T> output = new LinkedList<T>();
+        while(previous != null) {
+            output.add(previous.getValue());
+            previous = previous.getNext();
+        }
+
+        return output;
+    }
+
+    private static String receiveString(String message) {
+        System.out.print(message);
+        Scanner s = new Scanner(System.in);
+        return s.nextLine();
+    }
+
 }
