a140508c50a08cabb2fe15619c8fc1823768b40d
dolev goaz <www.dolev@gmail.com>
2024-04-12 18:02:40 +0300

    comments

--- src/q1/Main.java
+++ src/q1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static final int INPUT_COUNT = 6;
 
     public static void main(String[] args) {
-        LinkedList<String> list = receiveListFromUser();
+        LinkedList<String> list = receiveListFromUser(INPUT_COUNT);
         System.out.println(list);
         LinkedList<String> reversed = reverseList(list);
         System.out.println(reversed);
@@ -19,6 +19,7 @@ public class Main {
         System.out.println("THE OLDEST PERSON " + oldest);
     }
 
+    // Creates a list of people
     private static LinkedList<Person> createPeopleList() {
         LinkedList<Person> people = new LinkedList<Person>();
         people.add(new Person("Dolev Goaz", "123456789", 2001));
@@ -28,8 +29,10 @@ public class Main {
         return people;
     }
 
+    // Returns the maximum value of a list
     private static <T extends Comparable<T>> T max(LinkedList<T> list) {
         if (list.getHead() == null) {
+            // if list is empty, there's no max value
             return null;
         }
 
@@ -46,11 +49,14 @@ public class Main {
         return currentMax;
     }
 
+    // Reverses a linked list WITHOUT MAINTAINING THE SOURCE LIST
     private static <T> LinkedList<T> reverseList(LinkedList<T> source) {
         LinkedListItem<T> current = source.getHead();
         LinkedListItem<T> previous = null;
         LinkedListItem<T> next;
 
+        // logic- always 'store' the current 3 items. point the center one(the current) to the one before it(previous),
+        // then move them forward one step. we do that until we traversed the list
         while (current != null) {
             next = current.getNext();
             current.setNext(previous);
@@ -58,24 +64,30 @@ public class Main {
             current = next;
         }
 
-        // now previous is the new head
+        // now 'previous' is the new head
+        current = previous;
+        // this operation would be simple if there was a 'setHead' method, but it wasn't in the requirements.
+        // so, we loop at the new (makeshift) linked list and feed it into a new linked list.
         LinkedList<T> output = new LinkedList<T>();
-        while(previous != null) {
-            output.add(previous.getValue());
-            previous = previous.getNext();
+        while(current != null) {
+            output.add(current.getValue());
+            current = current.getNext();
         }
 
         return output;
     }
 
-    private static LinkedList<String> receiveListFromUser() {
+
+    // Receives k strings from the user and creates a linked list out of them.
+    private static LinkedList<String> receiveListFromUser(int k) {
         LinkedList<String> list = new LinkedList<String>();
-        for (int i = 0; i < INPUT_COUNT; i++) {
+        for (int i = 0; i < k; i++) {
             list.add(receiveString("Insert a string: "));
         }
         return list;
     }
 
+    // Receives a string from the user with the provided input message
     private static String receiveString(String message) {
         System.out.print(message);
         Scanner s = new Scanner(System.in);
